756a82e6f80a039cd8e7b2b1ada0db20fa1e0095
sognociel <sognociel@gmail.com>
2023-06-03 18:30:19 +0900

    [test] mock 함수 테스트 수정

--- src/test/MockFn.test.ts
+++ src/test/MockFn.test.ts
@@ -2,20 +2,28 @@ import { forEach } from "./MockFn";
 
 const mockCallback = jest.fn((x) => 42 + x);
 
-test("forEach mock function", () => {
-  forEach([0, 1], mockCallback);
+describe("Mock Test", () => {
+  beforeEach(() => {
+    forEach([0, 1], mockCallback);
+  });
 
   // mock 함수는 두 번 호출됨
-  expect(mockCallback.mock.calls).toHaveLength(2);
+  it("forEach mock function toHaveLength", () => {
+    expect(mockCallback.mock.calls).toHaveLength(2);
+  });
 
-  // The first argument of the first call to the function was 0
-  expect(mockCallback.mock.calls[0][0]).toBe(0);
+  // forEach의 [0][0]은 0
+  it("forEach mock function [0][0] toBe 0", () => {
+    expect(mockCallback.mock.calls[0][0]).toBe(0);
+  });
 
-  // The first argument of the second call to the function was 1
-  expect(mockCallback.mock.calls[1][0]).toBe(1);
+  // forEach의 [0][1]은 1
+  it("forEach mock function [0][1] toBe 1", () => {
+    expect(mockCallback.mock.calls[1][0]).toBe(1);
+  });
 
-  console.log(mockCallback.mock.results);
-
-  // // The return value of the first call to the function was 42
-  // expect(mockCallback.mock.results[0].value).toBe(42);
+  it("forEach mock function results", () => {
+    console.log(mockCallback.mock);
+    expect(mockCallback.mock.results[0].value).toBe(42);
+  });
 });
